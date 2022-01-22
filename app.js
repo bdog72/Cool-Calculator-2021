e8a1e683f4a8f517c3dc76746ff0b375e68dc4fc
Brian Bycynski <high5bri@hotmail.com>
2022-01-22 06:41:02 -0500

    calculator

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 //
 //
 
-console.log(123);
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+
+const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+  // If currentDisplay value is 0, replace it, if not add number
+
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === '0' ? number : displayValue + number;
+}
+
+console.log(inputBtns);
+
+// Add Event Listeners for numbers, operators and decimal buttons
+inputBtns.forEach(function (inputBtn) {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }
+
+  if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }
+
+  if (inputBtn.classList.contains('decimal')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }
+});
